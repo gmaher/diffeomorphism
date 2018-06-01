c2c26ac636daf3a7efaf285eab43cf8ad8722524
gabriel <gmaher2@hotmail.com>
2018-06-01 09:16:42 -0700

    optimization working

--- src/optimization.py
+++ src/optimization.py
@@ -18,35 +18,30 @@ def image_energy(I0, I1, X, U):
     E = np.nansum((I0_values - I1_values)**2)
     return E
 
-def diff_regularizer(U, X, Uint, dx, alpha=0.01, gamma=1):
+def diff_regularizer(U, X, Uint, dx, alpha=-0.01, gamma=1):
     N, d = U.shape
 
     R = np.zeros(U.shape)
 
     for i in range(d):
-        R += -alpha*interp_c_diff_2(Uint,X,dx, dimension=d)
+        R += alpha*interp_c_diff_2(Uint,X,dx, dimension=i)
 
     R += gamma*U
 
     return np.nansum(R**2)
 
-class EnergyFunction(object):
-    def __init__(self, I0, I1, X, Uint, dx, regularizers=[]):
-        self.I0 = I0
-        self.I1 = I1
-        self.X  = X
+class CostFunction(object):
+    def __init__(self, X, Uint, functions=[]):
+        self.X    = X
         self.Uint = Uint
-        self.dx = dx
-        self.regularizers = regularizers
+        self.functions = functions
 
-    def energy(self,U):
-        E  = 0
+    def __call__(self,U):
         Ur = U.reshape((self.X.shape))
 
         self.Uint.values = Ur
+        E = 0
+        for f in self.functions:
+            E += f(Ur)
 
-        for reg in self.regularizers:
-            E += reg(Ur)
-
-        E += image_energy(self.I0, self.I1, self.X, Ur)
         return E
--- test/image_interpolate.py
+++ test/image_interpolate.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath('..'))
 
-from src.optimization import EnergyFunction, diff_regularizer
+from src.optimization import CostFunction, diff_regularizer, image_energy
 
 from scipy.interpolate import LinearNDInterpolator
 from scipy import optimize
@@ -46,7 +46,7 @@ points_fine = np.concatenate((np.ravel(X_fine)[:,np.newaxis],
 F_fine = F_int(points_fine).reshape((Nfine,Nfine))
 
 Ux = SCALE*deformation(X_fine,Y_fine, MU[0], MU[1], SIGMA)
-Uy = -Ux[:,:]
+Uy = Ux[:,:]
 U = np.concatenate((np.ravel(Ux)[:,np.newaxis], np.ravel(Uy)[:,np.newaxis]),
     axis=1)
 
@@ -68,33 +68,63 @@ F_diff_int = LinearNDInterpolator(points_fine, F_int(Points_diff))
 ##############################################
 # optimization
 ##############################################
-U0   = np.zeros(points_fine.shape)
+U0     = np.zeros(points_fine.shape)
+U0_vec = np.ravel(U0)
+U0_int = LinearNDInterpolator(points_fine, U0)
+
 Ureg = U_int(points_fine)
-energy_function = EnergyFunction(F_int, F_diff_int, points_fine,
-    U_int, 1e-3)
 
-print(energy_function.energy(U0))
-print(energy_function.energy(Ureg))
+reg0 = diff_regularizer(U0, points_fine, U0_int, 1e-3)
+regU = diff_regularizer(Ureg, points_fine, U_int, 1e-3)
 
+f1 = lambda U: image_energy(F_int, F_diff_int, points_fine, U)
+f2 = lambda U: 0.001*diff_regularizer(U, points_fine, U_int, 1e-3)
+
+cost_function = CostFunction(points_fine, U_int, functions=[f1,f2])
+
+print(cost_function(np.ravel(U0)))
+print(cost_function(np.ravel(Ureg)))
+
+U_final = optimize.minimize(cost_function, U0, method="BFGS",
+    options={'disp':True}).x
+
+U_final = U_final.reshape(U_fine.shape)
+
+Points_final = np.concatenate((np.ravel(X_fine-U_final[:,:,0])[:,np.newaxis],
+    np.ravel(Y_fine-U_final[:,:,1])[:,np.newaxis]),axis=1)
+
+F_final = F_int(Points_final).reshape((Nfine,Nfine))
 ##############################################
 # Plot
 ##############################################
-f,axarr = plt.subplots(3,2)
+f,axarr = plt.subplots(5,2)
 p1 = axarr[0,0].imshow(U_coarse[:,:,0], cmap='rainbow')
 plt.colorbar(p1, ax=axarr[0,0])
 
 p2 = axarr[0,1].imshow(U_coarse[:,:,1], cmap='rainbow')
 plt.colorbar(p2, ax=axarr[0,1])
 
-p3 = axarr[1,0].imshow(U_fine[:,:,0], cmap='rainbow')
-plt.colorbar(p3, ax=axarr[1,0])
+p7 = axarr[1,0].imshow(U_final[:,:,0], cmap='rainbow')
+plt.colorbar(p7, ax=axarr[1,0])
 
-p4 = axarr[1,1].imshow(U_fine[:,:,1], cmap='rainbow')
-plt.colorbar(p4, ax=axarr[1,1])
+p8 = axarr[1,1].imshow(U_final[:,:,1], cmap='rainbow')
+plt.colorbar(p8, ax=axarr[1,1])
 
-p5 = axarr[2,0].imshow(F_fine, cmap="rainbow")
-plt.colorbar(p5, ax=axarr[2,0])
+p3 = axarr[2,0].imshow(U_fine[:,:,0], cmap='rainbow')
+plt.colorbar(p3, ax=axarr[2,0])
 
-p6 = axarr[2,1].imshow(F_diff, cmap='rainbow')
-plt.colorbar(p6, ax=axarr[2,1])
+p4 = axarr[2,1].imshow(U_fine[:,:,1], cmap='rainbow')
+plt.colorbar(p4, ax=axarr[2,1])
+
+p5 = axarr[3,0].imshow(F_fine, cmap="rainbow")
+plt.colorbar(p5, ax=axarr[3,0])
+
+p6 = axarr[3,1].imshow(F_diff, cmap='rainbow')
+plt.colorbar(p6, ax=axarr[3,1])
+
+p9 = axarr[4,0].imshow(F_final, cmap='rainbow')
+plt.colorbar(p9, ax=axarr[4,0])
+
+p10 = axarr[4,1].imshow(F_final, cmap='rainbow')
+plt.colorbar(p10, ax=axarr[4,1])
 plt.show()
